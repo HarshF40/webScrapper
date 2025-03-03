225a1fb0dcc58786124450db5a82ed65dc939085
HarshF40 <harshgaonkar57@gmail.com>
2025-03-03 08:59:06 +0530

    delArch

--- exPetrol.py
+++ exPetrol.py
@@ -1,19 +1,18 @@
 import re #for regex
 import json 
 
-def getPetrolPrice() :
-    with open("petrol.html", "r") as file : #open the file as file
-        data = file.read() #read the content in the file
+with open("petrol.html", "r") as file : #open the file as file
+    data = file.read() #read the content in the file
 
-    pattern = r'₹(\d+\.\d{2})' #regex to match the first occurance
-    match = re.search(pattern, data) #try to find a match for the regex pattern
+pattern = r'₹(\d+\.\d{2})' #regex to match the first occurance
+match = re.search(pattern, data) #try to find a match for the regex pattern
 
-    price = None
-    if match :
-        price = match.group(1)
-    else :
-        print("No match found")
+price = None
+if match :
+    price = match.group(1)
+else :
+    print("No match found")
 
-    if price :
-        with open("petrol_price.json", "w") as jsn :
-            jsn.write(json.dumps({"petrol_price" : float(price)}))
+if price :
+    with open("price.json", "w") as jsn :
+        jsn.write(json.dumps({"petrol_price" : int(price)}))
